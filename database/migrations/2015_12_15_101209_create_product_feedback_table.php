3e0bdb88fed5a3117179f51e3cdd7f4e4e17648b
agung69 <agung.bg69@gmail.com>
2015-12-17 15:59:21 +0700

    baru atur atur lagi yg migrationnya

--- database/migrations/2015_12_15_101209_create_product_feedback_table.php
+++ database/migrations/2015_12_15_101209_create_product_feedback_table.php
@@ -14,7 +14,23 @@ class CreateProductFeedbackTable extends Migration
     {
         Schema::create('product_feedback', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+            $table->integer('feedback_time')->unsigned();
+            $table->string('feedback_comment', 128);
+            $table->enum('feedback_type', array('P', 'N'));
+            $table->softDeletes();
             $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('user')
+                ->onDelete('cascade');
+
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('user')
+                ->onDelete('cascade');
         });
     }
 
