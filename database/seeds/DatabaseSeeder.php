5ac1015f2f0b77e81d388f78b76d0ebc4dd92ce7
agung69 <agung.bg69@gmail.com>
2016-01-18 11:39:34 +0700

    JWT-Auth login

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(UserLevelTableSeeder::class);
         $this->call(CategoryTableSeeder::class);
+        $this->call(UserTableSeeder::class);
 
         Model::reguard();
     }
