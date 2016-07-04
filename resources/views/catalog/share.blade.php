e266ae4a0dd4da52b83ca5c316f7184c78fa98f4
Riva'i Amin <raysaber37@gmail.com>
2016-07-04 23:57:36 +0700

    fix share blade

--- resources/views/catalog/share.blade.php
+++ resources/views/catalog/share.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta property="og:title" content="{{ $product->name; }}" />
-		<meta property="og:description" content="{{ $product->desc; }}" />
+		<meta property="og:title" content="{{ $product->name }}" />
+		<meta property="og:description" content="{{ $product->desc }}" />
 		<meta property="og:image" content="{{ $files }}/product/logo/{{ $product->logo }}" />
 		<!-- etc. -->
 	</head>
 	<body>
-		<p>{{ echo $product->desc; }}</p>
+		<p>{{ $product->desc }}</p>
 		<img src="{{ $files }}/product/logo/{{ $product->logo }}">
 	</body>
 </html>
