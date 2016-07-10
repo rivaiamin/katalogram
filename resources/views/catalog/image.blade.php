8f6310e2fb82b13ef981f2335fc3b4d4f91853b3
Riva'i Amin <raysaber37@gmail.com>
2016-07-10 13:09:16 +0700

    fixing page2image

--- resources/views/catalog/image.blade.php
+++ resources/views/catalog/image.blade.php
@@ -11,6 +11,6 @@
 		<!-- etc. -->
 	</head>
 	<body>
-		<img id="p2i_demo" src="http://api.page2images.com/directlink?p2i_url=http://katalogram.com/catalog/{{$product->id}}/view&p2i_device=6&p2i_screen=600x0&p2i_size=600x0&p2i_fullpage=1&p2i_imageformat=jpg&p2i_wait=5&p2i_key=f89b84457da1f2c2" />
+		<img id="p2i_demo" src="http://api.page2images.com/directlink?p2i_url=http://api.katalogram.com/catalog/{{$product->id}}/view&p2i_device=6&p2i_screen=600x0&p2i_size=600x0&p2i_fullpage=1&p2i_imageformat=jpg&p2i_wait=5&p2i_key=f89b84457da1f2c2" />
 	</body>
 </html>
